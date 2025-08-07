2ce3653f833575c5785ff91cd4b6f5f2b0db1667
Avinash Kumar <avku21ec@cmrit.ac.in>
2025-08-07 13:14:21 +0530

    added http server

--- server.js
+++ server.js
@@ -1,5 +1,64 @@
-import {createServer} from 'http';
-import {parse} from 'url';
+import { createServer } from 'http';
+import { parse } from 'url';
 import next from 'next';
 import StreamingServer from './lib/streaming_service.js'
+
+const dev = process.env.NODE_ENV !== "production"
+const hostName = 'localhost';
+const port = process.env.PORT || 3000
+
+const streamingServer = new StreamingServer();
+
+const app = next({ dev, hostName, port })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const httpServer = createServer(async (req, res) => {
+        try {
+            const parsedUrl = parse(req, true)
+            await handle(req, res, parsedUrl)
+        } catch (err) {
+            console.error("Error while handling")
+            res.statusCode(500)
+            res.end("internal server error")
+        }
+
+        global.streamingServer = streamingServer;
+
+        httpServer.listen(port, (err) => {
+            if (err) throw err;
+            console.log(`>Ready to listen on http://${hostName}:${port}`)
+            console.log("Streaming server initialized")
+        })
+
+        process.on("SIGTERM", () => {
+            console.log("SIGTERM received shutting down gracefully")
+
+            //shutting down all active streams
+            streamingServer.listActiveStreams().forEach(stream => {
+                streamingServer.stopStream(stream.streamKey)
+            });
+
+            httpServer.close(() => {
+                console.log("Server closed")
+                process.exit(0)
+            })
+        })
+
+        process.on("SIGINT", () => {
+            console.log("SIGTERM received shutting down gracefully")
+
+            //shutting down all active streams
+            streamingServer.listActiveStreams().forEach(stream => {
+                streamingServer.stopStream(stream.streamKey)
+            });
+
+            httpServer.close(() => {
+                console.log("Server closed")
+                process.exit(0)
+            })
+        })
+
+    })
+})
 
